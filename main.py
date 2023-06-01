9b94d758392038e3c9625895679114dddd9da734
bxtait <270147104@yoobeestudent.ac.nz>
2023-06-01 19:45:27 +1200

    commit2
    commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from pomoapp import create_app
 
 app = create_app()
--- pomoapp/views.py
+++ pomoapp/views.py
@@ -14,12 +14,12 @@ def home():
         note = request.form.get('note')#Gets the note from the HTML
 
         if len(note) < 1:
-            flash('Note is too short!', category='error')
+            flash('Enter a proper task!', category='error')
         else:
             new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note
-            db.session.add(new_note) #adding the note to the database
+            db.session.add(new_note) #adding the task to the database
             db.session.commit()
-            flash('Note added!', category='success')
+            flash('Task added', category='success')
 
     return render_template("home.html", user=current_user)
 
